095a26d2dbc1c89020138ce034137e03bdbb8fa4
leejh <leejh@biztechpartners.co.kr>
2019-04-02 11:53:18 +0900

    ch9

--- graphql/db.js
+++ graphql/db.js
@@ -1,56 +1,50 @@
-export const people = [
+let movies = [
+    {
+        id:0,
+        name: "Star Wars",
+        score: 1
+    },
     {
         id:1,
-        name: "nicolas",
-        age: 18,
-        gender: "female"
+        name: "Avengers",
+        score: 8
     },
     {
         id:2,
-        name: "a",
-        age: 18,
-        gender: "female"
+        name: "The Godfather",
+        score: 99
     },
     {
         id:3,
-        name: "b",
-        age: 18,
-        gender: "female"
-    },
-    {
-        id:4,
-        name: "c",
-        age: 18,
-        gender: "female"
-    },
-    {
-        id:5,
-        name: "d",
-        age: 18,
-        gender: "female"
-    },
-    {
-        id:6,
-        name: "e",
-        age: 18,
-        gender: "female"
-    },
-    {
-        id:7,
-        name: "f",
-        age: 18,
-        gender: "female"
-    },
-    {
-        id:8,
-        name: "g",
-        age: 18,
-        gender: "female"
+        name: "Logan",
+        score: 2
     }
 ];
 
+export const getMovies = () => movies;
+
 
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === id);
-    return filteredPeople[0];
+    const filteredMovie = movies.filter(movie => movie.id === id);
+    return filteredMovie[0];
+}
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id)
+    if (movies.length > cleanedMovies.length) {
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: '${movies.length + 1}',
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
 }
